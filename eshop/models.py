7dacade1561b7573325fb80b35a2a898d5acd060
YMOF <yasminath.ossn@eduge.ch>
2026-03-10 10:54:28 +0100

    ajt des infos du client sur la facture

--- eshop/models.py
+++ eshop/models.py
@@ -68,11 +68,6 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.FloatField(default=0) # On garde FloatField car ton Product.price est en Float
 
-    first_name = models.CharField(max_length=50, blank=True)
-    last_name = models.CharField(max_length=50, blank=True)
-    address = models.CharField(max_length=250, blank=True)
-    postal_code = models.CharField(max_length=20, blank=True)
-    city = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f"Commande {self.id} - {self.user.username}"
