8734e5f026a0a28d45f71260d7430b8caedd90bb
anthohn <anthonyhohn@gmail.com>
2026-03-11 14:22:38 +0100

    change username for e-mail, add password recovery

--- eshop/urls.py
+++ eshop/urls.py
@@ -8,6 +8,22 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='eshop/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='product_list'), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='eshop/password_reset_form.html',
+        email_template_name='eshop/password_reset_email.html',
+        subject_template_name='eshop/password_reset_subject.txt',
+        success_url='/password_reset/done/'
+    ), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='eshop/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='eshop/password_reset_confirm.html',
+        success_url='/reset/done/'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='eshop/password_reset_complete.html'
+    ), name='password_reset_complete'),
     path('profile/', views.profile, name='profile'),
     path('delete_account/', views.delete_account, name='delete_account'),
     # Modifier un avis
